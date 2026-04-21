e8c52c778ae94f5c954485f89fcebb1ad645df4d
Amer <amajdi@student.42amman.com>
2026-04-21 20:28:57 +0300

    render a flat colored sphere using ray tracing algorithm and change the coord range to be [-1, 1] in Y and [-1, 1] * aspect ration in X

--- render.c
+++ render.c
@@ -1,13 +1,39 @@
+#include <math.h>
+#include "vector.h"
 #include "minirt.h"
 
-unsigned int	gradient(t_coord *coord)
+static double	intersect(t_ray *ray, t_sphere *sphere)
 {
-	return (create_trgb(
-			0,
-			(int) 255.999 * coord->x,
-			(int) 255.999 * coord->y,
-			0
-		));
+	double		discriminant;
+	double		a;
+	double		b;
+	double		c;
+	t_vec3		oc;
+
+	oc = sub(ray->origin, sphere->origin);
+	a = dot(ray->direction, ray->direction);
+	b = 2 * dot(oc, ray->direction);
+	c = dot(oc, oc) - sphere->radius * sphere->radius;
+	discriminant = b * b - 4 * a * c;
+	if (discriminant < 0)
+		return (-1);
+	return ((-b - sqrt(discriminant)) / (2 * a));
+}
+
+static unsigned int	trace_ray(t_coord *coord)
+{
+	t_ray		ray;
+	t_sphere	sphere;
+	double		intersection;
+
+	ray.origin = (t_point3){0, 0, -2};
+	ray.direction = (t_vec3){(coord->x), (coord->y), 1};
+	sphere.origin = (t_point3){0, 0, 0};
+	sphere.radius = 0.5;
+	intersection = intersect(&ray, &sphere);
+	if (intersection < 0)
+		return (create_trgb(255, 0, 0, 0));
+	return (create_trgb(255, 0, 50, 150));
 }
 
 void	render(t_framebuf *framebuf)
@@ -16,8 +42,10 @@ void	render(t_framebuf *framebuf)
 	int		j;
 	t_pixel	pixel;
 	t_coord	coord;
+	double	aspect_ratio;
 
 	j = 0;
+	aspect_ratio = (double) framebuf->width / framebuf->height;
 	while (j < framebuf->height)
 	{
 		i = 0;
@@ -25,9 +53,9 @@ void	render(t_framebuf *framebuf)
 		{
 			pixel.x = i;
 			pixel.y = j;
-			coord.x = ((double) i / framebuf->width);
-			coord.y = 1 - ((double) j / framebuf->height);
-			pixel.color = gradient(&coord);
+			coord.x = (((double) i / framebuf->width) * 2 - 1) * aspect_ratio;
+			coord.y = 1 - ((double) j / framebuf->height) * 2;
+			pixel.color = trace_ray(&coord);
 			ft_mlx_put_pixel(framebuf, &pixel);
 			i++;
 		}
